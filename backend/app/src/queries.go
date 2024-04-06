8293a19ad73049b44c59ffca2698e5a6ff7b2c15
Logan Endes <82857250+Log45@users.noreply.github.com>
2024-04-05 23:12:51 -0400

    getuserbyid method

--- backend/app/src/queries.go
+++ backend/app/src/queries.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = ""
+	port     = 0
+	user     = ""
+	password = ""
+	dbname   = ""
+)
+
+func GetUserById(uid string) *sql.Rows {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM 'USER' WHERE UID = '%s'", uid)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	return rows
+}
