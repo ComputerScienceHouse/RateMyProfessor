b0117b9be97b7987b60d01e03405a806de4d93d4
Log45 <skyker604@gmail.com>
2024-07-21 03:22:43 -0500

    ADD: implementation for queries for each route

--- backend/app/src/handlers.go
+++ backend/app/src/handlers.go
@@ -36,34 +36,63 @@ func handleUsers(mux *http.ServeMux) *http.ServeMux {
 	// We could remove this since it is redundant to the search url
 	mux.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all users
+		var users []User
 		fmt.Fprintf(w, "This is the users page")
+		rows := GetUsers()
+		for rows.Next() {
+			var user User
+			err := rows.Scan(&user.Uid, &user.Name)
+			if err != nil {
+				fmt.Printf("Error scanning user: %s", err.Error())
+			}
+			users = append(users, user)
+		}
 	})
 
 	mux.HandleFunc("GET /users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the user with the given id
+		var user User
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the user page for user: %s", id)
+		rows := GetUserById(id)
+		rows.Next()
+		err := rows.Scan(&user.Uid, &user.Name)
+		if err != nil {
+			fmt.Printf("Error scanning user: %s", err.Error())
+		}
 	})
 
 	mux.HandleFunc("GET /users/{id}/reviews", func(w http.ResponseWriter, r *http.Request) {
 		// Return the reviews for the user with the given id
+		var reviews []Review
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the reviews page for user: %s", id)
+		rows := GetReviewsByUser(id)
+		for rows.Next() {
+			var review Review
+			err := rows.Scan(&review.Id, &review.Quality, &review.Difficulty, &review.Date, &review.Course, &review.Professor, &review.User)
+			if err != nil {
+				fmt.Printf("Error scanning review: %s", err.Error())
+			}
+			reviews = append(reviews, review)
+		}
 	})
+	// this is redundant
+	// mux.HandleFunc("GET /users/{id}/reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
+	// 	// Return the review with the given id for the user with the given id
+	// 	id := r.PathValue("id")
+	// 	rid := r.PathValue("rid")
+	// 	fmt.Fprintf(w, "This is the review page for user: %s and review: %s", id, rid)
+	// })
 
-	mux.HandleFunc("GET /users/{id}/reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
-		// Return the review with the given id for the user with the given id
-		id := r.PathValue("id")
-		rid := r.PathValue("rid")
-		fmt.Fprintf(w, "This is the review page for user: %s and review: %s", id, rid)
-	})
-
+	// TODO: this needs to be expanded on; i'm not sure how we can create a user without a name input
 	mux.HandleFunc("POST /users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Create a new user with the given id
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "Create a new user with id: %s", id)
 	})
 
+	// TODO: same here, there needs to be information to update
 	mux.HandleFunc("PUT /users/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Update the user with the given id
 		id := r.PathValue("id")
@@ -75,34 +104,65 @@ func handleUsers(mux *http.ServeMux) *http.ServeMux {
 func handleColleges(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("GET /colleges", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all colleges
+		var colleges []College
 		fmt.Fprintf(w, "This is the colleges page")
+		rows := GetColleges()
+		for rows.Next() {
+			var college College
+			err := rows.Scan(&college.Id, &college.Name)
+			if err != nil {
+				fmt.Printf("Error scanning college: %s", err.Error())
+			}
+			colleges = append(colleges, college)
+		}
 	})
 
 	mux.HandleFunc("GET /colleges/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the college with the given id
+		var college College
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the college page for college: %s", id)
+		rows := GetCollegeById(id)
+		rows.Next()
+		err := rows.Scan(&college.Id, &college.Name)
+		if err != nil {
+			fmt.Printf("Error scanning college: %s", err.Error())
+		}
 	})
 
 	mux.HandleFunc("GET /colleges/{id}/courses", func(w http.ResponseWriter, r *http.Request) {
 		// Return the courses for the college with the given id
+		var courses []Course
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the courses page for college: %s", id)
+		rows := GetCoursesByCollege(id)
+		for rows.Next() {
+			var course Course
+			err := rows.Scan(&course.Id, &course.Name, &course.Description, &course.College)
+			if err != nil {
+				fmt.Printf("Error scanning course: %s", err.Error())
+			}
+			courses = append(courses, course)
+		}
 	})
 
-	mux.HandleFunc("GET /colleges/{id}/courses/{cid}", func(w http.ResponseWriter, r *http.Request) {
-		// Return the course with the given id for the college with the given id
-		id := r.PathValue("id")
-		cid := r.PathValue("cid")
-		fmt.Fprintf(w, "This is the course page for college: %s and course: %s", id, cid)
-	})
+	// this is redundant to get college by id
+	// mux.HandleFunc("GET /colleges/{id}/courses/{cid}", func(w http.ResponseWriter, r *http.Request) {
+	// 	// Return the course with the given id for the college with the given id
 
+	// 	id := r.PathValue("id")
+	// 	cid := r.PathValue("cid")
+	// 	fmt.Fprintf(w, "This is the course page for college: %s and course: %s", id, cid)
+	// })
+
+	// TODO: expand this to add a name
 	mux.HandleFunc("POST /colleges/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Create a new college with the given id
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "Create a new college with id: %s", id)
 	})
 
+	// TODO: expand this to actually add information or disregard
 	mux.HandleFunc("PUT /colleges/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Update the college with the given id
 		id := r.PathValue("id")
@@ -114,34 +174,64 @@ func handleColleges(mux *http.ServeMux) *http.ServeMux {
 func handleCourses(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("GET /courses", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all courses
+		var courses []Course
 		fmt.Fprintf(w, "This is the courses page")
+		rows := GetCourses()
+		for rows.Next() {
+			var course Course
+			err := rows.Scan(&course.Id, &course.Name, &course.Description, &course.College)
+			if err != nil {
+				fmt.Printf("Error scanning course: %s", err.Error())
+			}
+			courses = append(courses, course)
+		}
 	})
 
 	mux.HandleFunc("GET /courses/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the course with the given id
+		var course Course
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the course page for course: %s", id)
+		rows := GetCourseById(id)
+		rows.Next()
+		err := rows.Scan(&course.Id, &course.Name, &course.Description, &course.College)
+		if err != nil {
+			fmt.Printf("Error scanning course: %s", err.Error())
+		}
 	})
 
 	mux.HandleFunc("GET /courses/{id}/reviews", func(w http.ResponseWriter, r *http.Request) {
 		// Return the reviews for the course with the given id
+		var reviews []Review
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the reviews page for course: %s", id)
+		rows := GetReviewsByCourse(id)
+		for rows.Next() {
+			var review Review
+			err := rows.Scan(&review.Id, &review.Quality, &review.Difficulty, &review.Date, &review.Course, &review.Professor, &review.User)
+			if err != nil {
+				fmt.Printf("Error scanning review: %s", err.Error())
+			}
+			reviews = append(reviews, review)
+		}
 	})
 
-	mux.HandleFunc("GET /courses/{id}/reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
-		// Return the review with the given id for the course with the given id
-		id := r.PathValue("id")
-		rid := r.PathValue("rid")
-		fmt.Fprintf(w, "This is the review page for course: %s and review: %s", id, rid)
-	})
+	// this is redundant to get review by id
+	// mux.HandleFunc("GET /courses/{id}/reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
+	// 	// Return the review with the given id for the course with the given id
+	// 	id := r.PathValue("id")
+	// 	rid := r.PathValue("rid")
+	// 	fmt.Fprintf(w, "This is the review page for course: %s and review: %s", id, rid)
+	// })
 
+	// TODO: expand this to add a name and description
 	mux.HandleFunc("POST /courses/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Create a new course with the given id
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "Create a new course with id: %s", id)
 	})
 
+	// TODO: same here, there needs to be information to update
 	mux.HandleFunc("PUT /courses/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Update the course with the given id
 		id := r.PathValue("id")
@@ -153,21 +243,40 @@ func handleCourses(mux *http.ServeMux) *http.ServeMux {
 func handleReviews(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("GET /reviews", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all reviews
+		var reviews []Review
 		fmt.Fprintf(w, "This is the reviews page")
+		rows := GetReviews()
+		for rows.Next() {
+			var review Review
+			err := rows.Scan(&review.Id, &review.Quality, &review.Difficulty, &review.Date, &review.Course, &review.Professor, &review.User)
+			if err != nil {
+				fmt.Printf("Error scanning review: %s", err.Error())
+			}
+			reviews = append(reviews, review)
+		}
 	})
 
 	mux.HandleFunc("GET /reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the review with the given id
+		var review Review
 		rid := r.PathValue("rid")
 		fmt.Fprintf(w, "This is the review page for review: %s", rid)
+		rows := GetReviewById(rid)
+		rows.Next()
+		err := rows.Scan(&review.Id, &review.Quality, &review.Difficulty, &review.Date, &review.Course, &review.Professor, &review.User)
+		if err != nil {
+			fmt.Printf("Error scanning review: %s", err.Error())
+		}
 	})
 
+	// TODO: expand this to add a quality, difficulty, date, course, professor, and user
 	mux.HandleFunc("POST /reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
 		// Create a new review with the given id
 		rid := r.PathValue("rid")
 		fmt.Fprintf(w, "Create a new review with id: %s", rid)
 	})
 
+	// TODO: same here, there needs to be information to update
 	mux.HandleFunc("PUT /reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
 		// Update the review with the given id
 		rid := r.PathValue("rid")
@@ -178,6 +287,8 @@ func handleReviews(mux *http.ServeMux) *http.ServeMux {
 		// Delete the review with the given id
 		rid := r.PathValue("rid")
 		fmt.Fprintf(w, "This is the delete review page for review: %s", rid)
+		DeleteReview(rid)
+
 	})
 	return mux
 }
@@ -190,46 +301,126 @@ func handleSearch(mux *http.ServeMux) *http.ServeMux {
 
 	mux.HandleFunc("GET /search/users", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all users
+		var users []User
 		fmt.Fprintf(w, "This is the search page for users")
+		rows := GetUsers()
+		for rows.Next() {
+			var user User
+			err := rows.Scan(&user.Uid, &user.Name)
+			if err != nil {
+				fmt.Printf("Error scanning user: %s", err.Error())
+			}
+			users = append(users, user)
+		}
 	})
 
 	mux.HandleFunc("GET /search/users/{name}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of users with the given name
+		var users []User
 		name := r.PathValue("name")
 		fmt.Fprintf(w, "This is the search page for users with name: %s", name)
+		rows := GetUsersByName(name)
+		for rows.Next() {
+			var user User
+			err := rows.Scan(&user.Uid, &user.Name)
+			if err != nil {
+				fmt.Printf("Error scanning user: %s", err.Error())
+			}
+			users = append(users, user)
+		}
 	})
 
 	mux.HandleFunc("GET /search/professors", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all professors
+		var professors []Professor
 		fmt.Fprintf(w, "This is the search page for professors")
+		rows := GetProfessors()
+		for rows.Next() {
+			var professor Professor
+			err := rows.Scan(&professor.Id, &professor.Name)
+			if err != nil {
+				fmt.Printf("Error scanning professor: %s", err.Error())
+			}
+			professors = append(professors, professor)
+		}
 	})
 
 	mux.HandleFunc("GET /search/professors/{name}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of professors with the given name
+		var professors []Professor
 		name := r.PathValue("name")
 		fmt.Fprintf(w, "This is the search page for professors with name: %s", name)
+		rows := GetProfessorsByName(name)
+		for rows.Next() {
+			var professor Professor
+			err := rows.Scan(&professor.Id, &professor.Name)
+			if err != nil {
+				fmt.Printf("Error scanning professor: %s", err.Error())
+			}
+			professors = append(professors, professor)
+		}
 	})
 
 	mux.HandleFunc("GET /search/courses", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all courses
+		var courses []Course
 		fmt.Fprintf(w, "This is the search page for courses")
+		rows := GetCourses()
+		for rows.Next() {
+			var course Course
+			err := rows.Scan(&course.Id, &course.Name, &course.Description, &course.College)
+			if err != nil {
+				fmt.Printf("Error scanning course: %s", err.Error())
+			}
+			courses = append(courses, course)
+		}
 	})
 
 	mux.HandleFunc("GET /search/courses/{name}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of courses with the given name
+		var courses []Course
 		name := r.PathValue("name")
 		fmt.Fprintf(w, "This is the search page for courses with name: %s", name)
+		rows := GetCoursesByName(name)
+		for rows.Next() {
+			var course Course
+			err := rows.Scan(&course.Id, &course.Name, &course.Description, &course.College)
+			if err != nil {
+				fmt.Printf("Error scanning course: %s", err.Error())
+			}
+			courses = append(courses, course)
+		}
 	})
 
 	mux.HandleFunc("GET /search/colleges", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all colleges
+		var colleges []College
 		fmt.Fprintf(w, "This is the search page for colleges")
+		rows := GetColleges()
+		for rows.Next() {
+			var college College
+			err := rows.Scan(&college.Id, &college.Name)
+			if err != nil {
+				fmt.Printf("Error scanning college: %s", err.Error())
+			}
+			colleges = append(colleges, college)
+		}
 	})
 
 	mux.HandleFunc("GET /search/colleges/{name}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of colleges with the given name
+		var colleges []College
 		name := r.PathValue("name")
 		fmt.Fprintf(w, "This is the search page for colleges with name: %s", name)
+		rows := GetCollegesByName(name)
+		for rows.Next() {
+			var college College
+			err := rows.Scan(&college.Id, &college.Name)
+			if err != nil {
+				fmt.Printf("Error scanning college: %s", err.Error())
+			}
+			colleges = append(colleges, college)
+		}
 	})
 	// I am choosing not to implement search by reviews because that seems backwards
 	return mux
@@ -238,34 +429,64 @@ func handleSearch(mux *http.ServeMux) *http.ServeMux {
 func handleProfessors(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("GET /professors", func(w http.ResponseWriter, r *http.Request) {
 		// Return the list of all professors
+		var professors []Professor
 		fmt.Fprintf(w, "This is the professors page")
+		rows := GetProfessors()
+		for rows.Next() {
+			var professor Professor
+			err := rows.Scan(&professor.Id, &professor.Name)
+			if err != nil {
+				fmt.Printf("Error scanning professor: %s", err.Error())
+			}
+			professors = append(professors, professor)
+		}
 	})
 
 	mux.HandleFunc("GET /professors/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Return the professor with the given id
+		var professor Professor
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the professor page for professor: %s", id)
+		rows := GetProfessorById(id)
+		rows.Next()
+		err := rows.Scan(&professor.Id, &professor.Name)
+		if err != nil {
+			fmt.Printf("Error scanning professor: %s", err.Error())
+		}
 	})
 
 	mux.HandleFunc("GET /professors/{id}/reviews", func(w http.ResponseWriter, r *http.Request) {
 		// Return the reviews for the professor with the given id
+		var reviews []Review
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "This is the reviews page for professor: %s", id)
+		rows := GetReviewsByProfessor(id)
+		for rows.Next() {
+			var review Review
+			err := rows.Scan(&review.Id, &review.Quality, &review.Difficulty, &review.Date, &review.Course, &review.Professor, &review.User)
+			if err != nil {
+				fmt.Printf("Error scanning review: %s", err.Error())
+			}
+			reviews = append(reviews, review)
+		}
 	})
 
-	mux.HandleFunc("GET /professors/{id}/reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
-		// Return the review with the given id for the professor with the given id
-		id := r.PathValue("id")
-		rid := r.PathValue("rid")
-		fmt.Fprintf(w, "This is the review page for professor: %s and review: %s", id, rid)
-	})
+	// this is redundant
+	// mux.HandleFunc("GET /professors/{id}/reviews/{rid}", func(w http.ResponseWriter, r *http.Request) {
+	// 	// Return the review with the given id for the professor with the given id
+	// 	id := r.PathValue("id")
+	// 	rid := r.PathValue("rid")
+	// 	fmt.Fprintf(w, "This is the review page for professor: %s and review: %s", id, rid)
+	// })
 
+	// TODO: this needs to be expanded on; i'm not sure how we can create a professor without a name input
 	mux.HandleFunc("POST /professors/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Create a new professor with the given id
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "Create a new professor with id: %s", id)
 	})
 
+	// TODO: same here, there needs to be information to update
 	mux.HandleFunc("PUT /professors/{id}", func(w http.ResponseWriter, r *http.Request) {
 		// Update the professor with the given id
 		id := r.PathValue("id")
--- backend/app/src/queries.go
+++ backend/app/src/queries.go
@@ -32,3 +32,199 @@ func GetUsersByName(name string) *sql.Rows {
 	}
 	return rows
 }
+
+func GetUsers() *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'users'")
+	if err != nil {
+		fmt.Printf("Error getting all users: %s", err.Error())
+	}
+	return rows
+}
+
+func DeleteUser(uid string) {
+	_, err := pool.Query("DELETE FROM 'users' WHERE uid = '%s'", uid)
+	if err != nil {
+		fmt.Printf("Error deleting user with uid %s: %s", uid, err.Error())
+	}
+}
+
+func CreateCollege(id string, name string) *sql.Rows {
+	rows, err := pool.Query("INSERT INTO 'colleges' (id, name) VALUES ('%s', '%s')", id, name)
+	if err != nil {
+		fmt.Printf("Error creating college with name %s: %s", name, err.Error())
+	}
+	return rows
+}
+
+func GetCollegeById(id string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'colleges' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error getting college from id %s: %s", id, err.Error())
+	}
+	return rows
+}
+
+func GetCollegesByName(name string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'colleges' WHERE name LIKE %'%s'%", name)
+	if err != nil {
+		fmt.Printf("Error getting colleges from name %s: %s", name, err.Error())
+	}
+	return rows
+}
+
+func GetColleges() *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'colleges'")
+	if err != nil {
+		fmt.Printf("Error getting all colleges: %s", err.Error())
+	}
+	return rows
+}
+
+func DeleteCollege(id string) {
+	_, err := pool.Query("DELETE FROM 'colleges' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error deleting college with id %s: %s", id, err.Error())
+	}
+}
+
+func CreateCourse(id string, name string, description string, collegeId string) *sql.Rows {
+	rows, err := pool.Query("INSERT INTO 'courses' (id, name, description, college) VALUES ('%s', '%s', '%s', '%s')", id, name, description, collegeId)
+	if err != nil {
+		fmt.Printf("Error creating course with name %s: %s", name, err.Error())
+	}
+	return rows
+}
+
+func GetCourseById(id string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'courses' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error getting course from id %s: %s", id, err.Error())
+	}
+	return rows
+}
+
+func GetCoursesByName(name string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'courses' WHERE name LIKE %'%s'%", name)
+	if err != nil {
+		fmt.Printf("Error getting courses from name %s: %s", name, err.Error())
+	}
+	return rows
+}
+
+func GetCoursesByCollege(collegeId string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'courses' WHERE college = '%s'", collegeId)
+	if err != nil {
+		fmt.Printf("Error getting courses from college id %s: %s", collegeId, err.Error())
+	}
+	return rows
+}
+
+func GetCourses() *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'courses'")
+	if err != nil {
+		fmt.Printf("Error getting all courses: %s", err.Error())
+	}
+	return rows
+}
+
+func DeleteCourse(id string) {
+	_, err := pool.Query("DELETE FROM 'courses' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error deleting course with id %s: %s", id, err.Error())
+	}
+}
+
+func CreateReview(id string, quality float32, difficulty float32, date int, courseId string, professorId string, userId string) *sql.Rows {
+	rows, err := pool.Query("INSERT INTO 'reviews' (id, quality, difficulty, date, course, professor, user) VALUES ('%s', '%f', '%f', '%d', '%s', '%s', '%s')", id, quality, difficulty, date, courseId, professorId, userId)
+	if err != nil {
+		fmt.Printf("Error creating review with id %s: %s", id, err.Error())
+	}
+	return rows
+}
+
+func GetReviewById(id string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'reviews' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error getting review from id %s: %s", id, err.Error())
+	}
+	return rows
+}
+
+func GetReviewsByCourse(courseId string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'reviews' WHERE course = '%s'", courseId)
+	if err != nil {
+		fmt.Printf("Error getting reviews from course id %s: %s", courseId, err.Error())
+	}
+	return rows
+}
+
+func GetReviewsByProfessor(professorId string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'reviews' WHERE professor = '%s'", professorId)
+	if err != nil {
+		fmt.Printf("Error getting reviews from professor id %s: %s", professorId, err.Error())
+	}
+	return rows
+}
+
+func GetReviewsByUser(userId string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'reviews' WHERE user = '%s'", userId)
+	if err != nil {
+		fmt.Printf("Error getting reviews from user id %s: %s", userId, err.Error())
+	}
+	return rows
+}
+
+func GetReviews() *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'reviews'")
+	if err != nil {
+		fmt.Printf("Error getting all reviews: %s", err.Error())
+	}
+	return rows
+}
+
+func DeleteReview(id string) {
+	_, err := pool.Query("DELETE FROM 'reviews' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error deleting review with id %s: %s", id, err.Error())
+	}
+
+}
+
+func CreateProfessor(id string, name string) *sql.Rows {
+	rows, err := pool.Query("INSERT INTO 'professors' (id, name) VALUES ('%s', '%s')", id, name)
+	if err != nil {
+		fmt.Printf("Error creating professor with name %s: %s", name, err.Error())
+	}
+	return rows
+}
+
+func GetProfessorById(id string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'professors' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error getting professor from id %s: %s", id, err.Error())
+	}
+	return rows
+}
+
+func GetProfessorsByName(name string) *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'professors' WHERE name LIKE %'%s'%", name)
+	if err != nil {
+		fmt.Printf("Error getting professors from name %s: %s", name, err.Error())
+	}
+	return rows
+}
+
+func GetProfessors() *sql.Rows {
+	rows, err := pool.Query("SELECT * FROM 'professors'")
+	if err != nil {
+		fmt.Printf("Error getting all professors: %s", err.Error())
+	}
+	return rows
+}
+
+func DeleteProfessor(id string) {
+	_, err := pool.Query("DELETE FROM 'professors' WHERE id = '%s'", id)
+	if err != nil {
+		fmt.Printf("Error deleting professor with id %s: %s", id, err.Error())
+	}
+}
